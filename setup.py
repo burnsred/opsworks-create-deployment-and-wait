9bd03045602019fc835f97a948c78b359b5ce250
bassrock <bassrock@users.noreply.github.com>
2014-10-07 11:48:02 -0700

    Upping the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='create-opsworks-deployment-and-wait',
-    version='0.0.1',
+    version='0.0.2',
     packages=['opsworks_create_deployment_and_wait',],
     scripts=['bin/opsworks-create-deployment-and-wait'],
     include_package_data=True,
